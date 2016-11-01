35707bb14d00003aabe470c623dbf756464e0659
David V <gtdavidv@gmail.com>
2016-10-31 23:13:13 -0500

    Added semantic graph db models

--- app.py
+++ app.py
@@ -15,6 +15,9 @@ Database setup
 
 db = SQLAlchemy(app)
 
+"""
+" The main chat model - where the user's submissions and the bot's response are stored
+"""
 class message(db.Model):
 	id = db.Column('message_id', db.Integer, primary_key = True)
 	text = db.Column(db.Text)
@@ -25,6 +28,9 @@ class message(db.Model):
 		self.text = text
 		self.session_id = session_id
 
+"""
+" Training chats model
+"""
 class training_chats(db.Model):
 	id = db.Column('message_id', db.Integer, primary_key = True)
 	chat_id = db.Column(db.Integer)
@@ -37,6 +43,32 @@ class training_chats(db.Model):
 		self.praty = party
 		self.text = text
 		self.node_id = node_id
+
+"""
+" Model for the nodes of the semantic graph
+"""
+class semantic_graph_nodes(db.Model):
+	id = db.Column('node_id', db.Integer, primary_key = True)
+	title = db.Column(db.String(255))
+
+	def __init__(self, title):
+		self.title = title
+
+"""
+" Model for the relationships that connect the nodes of the semantic graph
+"""
+class semantic_graph_relationships(db.Model):
+	id = db.Column('relationship_id', db.Integer, primary_key = True)
+	node_id_1 = db.Column(db.Integer)
+	node_id_2 = db.Column(db.Integer)
+	relationship_1_to_2 = db.Column(db.String(255))
+	relationship_2_to_1 = db.Column(db.String(255))
+
+	def __init__(self, node_id_1, node_id_2, relationship_1_to_2, relationship_2_to_1):
+		self.node_id_1 = node_id_1
+		self.node_id_2 = node_id_2
+		self.relationship_1_to_2 = relationship_1_to_2
+		self.relationship_2_to_1 = relationship_2_to_1
 	   
 db.create_all()
 
